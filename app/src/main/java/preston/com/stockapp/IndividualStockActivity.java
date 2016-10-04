403c506a78e77741c3891151ca26e5ba19c184f2
wags906 <alexpreston117@gmail.com>
2016-10-04 11:43:07 -0700

    Added the invidiual stock page and started formating

---
+++ app/src/main/java/preston/com/stockapp/IndividualStockActivity.java
@@ -0,0 +1,18 @@
+package preston.com.stockapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Alex Preston on 10/4/16.
+ */
+
+public class IndividualStockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.invididual_stock_acitivity);
+    }
+}
