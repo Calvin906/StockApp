a7ea1c33a42fb17fe69fe9e1fd19886afce04638
wags906 <alexpreston117@gmail.com>
2016-10-29 11:50:00 -0700

    Working on adding a google sign in

--- app/src/main/java/preston/com/stockapp/SignInActivity.java
+++ app/src/main/java/preston/com/stockapp/SignInActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 
@@ -20,20 +22,19 @@ import com.google.android.gms.common.api.GoogleApiClient;
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
     private TextView signIn;
-    private TextView cancel;
     private GoogleSignInOptions gso;
     private GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 9001;
+    private static final String TAG = String.format("tag.%s", SignInActivity.class.getName());
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         signIn = (TextView) findViewById(R.id.sign_in_button_si);
-        cancel = (TextView) findViewById(R.id.cancel_button_si);
 
 
         signIn.setOnClickListener(this);
-        cancel.setOnClickListener(this);
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -47,29 +48,60 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
                 .build();
 
 
-
-
-
     }
 
+    /**
+     * Method implemented by onClickListener. Handles actions of clicks
+     *
+     * @param v
+     */
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.cancel_button_si:
-                cancelAction();
             case R.id.sign_in_button_si:
-
+                signIn();
 
         }
     }
 
     /**
-     * Cancel method to go back to the main menu
+     * On the activity result, if the request code is the RC code, then it will create a google signIn result
+     * and check with teh handleSignInResult Method to see if it was successful
+     * @param requestCode
+     * @param resultCode
+     * @param data
      */
-    public void cancelAction() {
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResults(result);
+        }
     }
+
+    /**
+     * This method checks to see if the sign in result is successful
+     *
+     * @param result
+     */
+    private void handleSignInResults(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult: " + result.isSuccess() );
+        if (result.isSuccess()) {
+            //Sign in is successful, show UI
+            
+        }
+
+    }
+
+    /**
+     * Launch the sign In method
+     */
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
