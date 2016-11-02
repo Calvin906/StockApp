242a75d1b8220285e01f933d7a0938e77205b4a5
wags906 <alexpreston117@gmail.com>
2016-11-01 18:40:00 -0700

    Deleted SQL database, Gonna use Greendoa

--- app/src/main/java/preston/com/stockapp/PortfolioActivity.java
+++ app/src/main/java/preston/com/stockapp/PortfolioActivity.java
@@ -1,7 +1,12 @@
 package preston.com.stockapp;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import preston.com.stockapp.util.PortfolioRecyclerViewAdapter;
 
 /**
  * Created by Alex Preston on 9/21/16.
@@ -9,9 +14,27 @@ import android.os.Bundle;
 
 public class PortfolioActivity extends Activity {
 
+    private RecyclerView pRecyclerView;
+    private PortfolioRecyclerViewAdapter pAdapter;
+    private SQLiteDatabase database;
+    private FeederReaderContract.FeederReaderDBHelper mDbHelper;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.portfolio_activity);
+
+        //Initialize RecyclerView, and adapter, and Layoutmanager
+        pRecyclerView = (RecyclerView) findViewById(R.id.portfolio_recycler_view);
+        pAdapter = new PortfolioRecyclerViewAdapter();
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        //Set adapter and LayoutManager
+        pRecyclerView.setAdapter(pAdapter);
+        pRecyclerView.setLayoutManager(llm);
     }
+
+
 }
